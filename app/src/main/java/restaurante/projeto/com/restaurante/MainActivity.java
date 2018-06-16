0bf5de12fd6478b729b70795f07d90c70d0e4ba0
Jose Augusto <j.augustocss@gmail.com>
2018-06-15 23:22:34 -0300

    Atualização de validação

--- app/src/main/java/restaurante/projeto/com/restaurante/MainActivity.java
+++ app/src/main/java/restaurante/projeto/com/restaurante/MainActivity.java
@@ -2,6 +2,7 @@ package restaurante.projeto.com.restaurante;
 
 import android.annotation.TargetApi;
 import android.icu.text.DecimalFormat;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
@@ -11,6 +12,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText consumoTotal;
     private EditText courverArtistico;
     private EditText quantidadePessoas;
-
     private EditText taxaServico;
     private EditText totalConta;
     private EditText valorPorPessoa;
@@ -32,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setIcon(R.mipmap.nova);
 
 
-        consumoTotal = findViewById(R.id.editConsumo_Id);
+         consumoTotal = findViewById(R.id.editConsumo_Id);
         courverArtistico = findViewById(R.id.editCouvert_Id);
         calcularConta = findViewById(R.id.calcularContaF_Id);
         quantidadePessoas = findViewById(R.id.editDividirPorP_Id);
@@ -47,21 +48,30 @@ public class MainActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
+                if(consumoTotal.getText().toString().equals("") || courverArtistico.getText().toString().equals("")
+                        || quantidadePessoas.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this,"Obrigatório preencher todos os campos!!",Toast.LENGTH_SHORT).show();
 
-                double consumo = Double.parseDouble(consumoTotal.getText().toString());
-                double couver = Double.parseDouble(courverArtistico.getText().toString());
-                int qtdPessoas = Integer.parseInt(quantidadePessoas.getText().toString());
+                }else if(Integer.parseInt(quantidadePessoas.getText().toString())==0){
+                    Toast.makeText(MainActivity.this, "Deve haver pelo menos uma pessoa!", Toast.LENGTH_LONG).show();
+                }else{
+                    double consumo = Double.parseDouble(consumoTotal.getText().toString());
+                    double couver = Double.parseDouble(courverArtistico.getText().toString());
+                    int qtdPessoas = Integer.parseInt(quantidadePessoas.getText().toString());
 
-                double taxa = ((consumo + couver) * 10) / 100;
-                double contaTotal = consumo + couver+taxa;
+                    double taxa = ((consumo + couver) * 10) / 100;
+                    double contaTotal = consumo + couver + taxa;
 
-                double totalPorPessoa = contaTotal / qtdPessoas;
-                DecimalFormat df = new DecimalFormat("###.##");
+                    double totalPorPessoa = contaTotal / qtdPessoas;
+                    DecimalFormat df = new DecimalFormat("###.##");
 
 
-                taxaServico.setText(String.format(df.format(taxa)));
-                totalConta.setText(String.format(df.format(contaTotal)));
-                valorPorPessoa.setText(String.format(df.format(totalPorPessoa)));
+                    taxaServico.setText(String.format(df.format(taxa)));
+                    totalConta.setText(String.format(df.format(contaTotal)));
+                    valorPorPessoa.setText(String.format(df.format(totalPorPessoa)));
+
+
+                }
 
 
             }
